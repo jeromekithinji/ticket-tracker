8f1fd3789e09c02c78b7da7fbbeaa5650b7ca520
jeromekithinji <kithinjijerome@gmail.com>
2021-09-14 14:45:00 +0100

    Initial commit

---
+++ ticket-tracker/src/components/Employee/Employee.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Employee.scss"
+
+const Employee = () => {
+    return (
+        <div className="ticket">
+            <h2>Name</h2>
+            <h2>Role</h2>
+            <div className="ticket__counter">
+                
+            </div>
+        </div>
+    )
+}
+
+export default Employee
